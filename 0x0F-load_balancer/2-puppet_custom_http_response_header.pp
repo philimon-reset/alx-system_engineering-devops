aaf56ed655958777cc77d19985488ac49e27393e
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-08-23 21:54:59 +0300

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -7,7 +7,7 @@ package {'nginx':
 
 file_line {'add header':
   path => '/etc/nginx/sites-available/default',
-  after => 'root /var/www/html;',
+  after => 'server_name _;',
   line => "\\\n\tadd_header X-Served-By \$HOSTNAME;\n",
 }
 
