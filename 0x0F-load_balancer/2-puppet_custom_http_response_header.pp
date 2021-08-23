6e9a37517ba9289a75f6f730840b5eba196a53e1
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-08-23 18:44:42 +0300

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,2 +1,27 @@
 #!/usr/bin/env bash
-#
+# setup nginx
+
+package {'nginx':
+  ensure  => installed,
+}
+
+file {'/var/www/html/index.nginx-debian.html':
+  content => 'Holberton School for the win!',
+}
+
+file_line {'configure redirection':
+  path => '/etc/nginx/sites-available/default',
+  after => 'server_name _;',
+  line => "\n\tlocation /redirect_me {\n\t\treturn 301 https://youtu.be/dQw4w9WgXcQ;\n\t}\n",
+}
+
+file_line {'add header':
+  path => '/etc/nginx/sites-available/default',
+  after => 'root /var/www/html;',
+  line => "\\\n\tadd_header X-Served-By \$HOSTNAME;\n",
+}
+
+
+service {'nginx':
+  ensure  => running,
+}
