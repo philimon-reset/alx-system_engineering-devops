a3b1e4a299c778b74eb0782e27275efdc606948d
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-08-24 06:22:29 +0300

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,6 @@
 # setup nginx
 
-package {'nginx':
+package { 'nginx':
   ensure  => installed,
 }
 
@@ -8,21 +8,22 @@ file { '/var/www/html/index.html':
   content => 'Holberton School',
 }
 
-file_line { 'configure redirect':
+file_line { 'writes redirection site':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-file_line {'add header':
-  path => '/etc/nginx/sites-available/default',
+file_line { 'add HTTP header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line => 'add_header X-Served-By $hostname;',
+  line   => 'add_header X-Served-By $hostname;',
 }
 
-service {'nginx':
-  ensure  => running,
-  enable => True,
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
   require => Package['nginx'],
 }
