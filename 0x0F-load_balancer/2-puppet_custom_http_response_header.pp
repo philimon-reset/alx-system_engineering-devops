616331b71c94c4d2164999862af40961223d5977
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-08-24 06:20:45 +0300

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,6 +4,17 @@ package {'nginx':
   ensure  => installed,
 }
 
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+}
+
+file_line { 'configure redirect':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
 file_line {'add header':
   path => '/etc/nginx/sites-available/default',
   after => 'root /var/www/html;',
