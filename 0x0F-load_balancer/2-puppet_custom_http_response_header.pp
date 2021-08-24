bcd94e63cc3717bc73a66621abcc00ad0c6ad3db
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-08-24 06:24:05 +0300

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,31 +1,17 @@
-# Manifest that install Nginx and configures:
-#   a simple default page.
-#   a custom header with the server hostname.
+# setup nginx
 
-package { 'nginx':
+package {'nginx':
   ensure  => installed,
 }
 
-file { '/var/www/html/index.html':
-  content => 'Holberton School',
+file_line {'add header':
+  path => '/etc/nginx/sites-available/default',
+  after => 'root /var/www/html;',
+  line => 'add_header X-Served-By $hostname;',
 }
 
-file_line { 'writes redirection site':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
-file_line { 'add HTTP header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'add_header X-Served-By $hostname;',
-}
-
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
+service {'nginx':
+  ensure  => running,
+  enable => True,
   require => Package['nginx'],
 }
