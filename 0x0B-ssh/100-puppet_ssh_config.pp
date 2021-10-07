1148fd5266a215fdc53150764f78be41700cda06
philimon-reset <philimonderib2019@gmail.com>
2021-10-07 12:55:57 +0300

    a #

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,11 @@
-# puppet
-file_line {"Declare identity file":
-  ensure => present,
-  path => "/etc/ssh/ssh_config",
-  line => "IdentityFile ~/.ssh/holberton"
+# configure ssh config
+
+file_line {'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
 }
 
-file_line {"Turn off passwd auth":
-  ensure => present,
-  path => "/etc/ssh/ssh_config",
-  line => "PasswordAuthentication no"
+file_line {'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
 }
