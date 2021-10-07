4beef86d846f423817c9622feaf75e7380afed47
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-10-07 10:57:32 +0300

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,6 @@
 # puppet allow user and expand file descriptor limit
 
-file_line {'configure redirection':
-  path  => '/etc/security/limits.conf',
-  after => '# /etc/security/limits.conf',
-  line  => "session required pam_limits.so\nholberton hard nofile 5000\nholberton soft nofile 4000",
+file {'/etc/security/limits.conf':
+  ensure  => present,
+  content => "session required pam_limits.so\nholberton hard nofile 5000\nholberton soft nofile 4000",
 }
